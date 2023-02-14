6693dc76b643d3b561a451e3f558c7e7f3632978
Mandeepsheoran <70449523+Mandeepsheoran@users.noreply.github.com>
2023-02-14 17:05:46 +0400

    Update DataProviderUtils.java

--- src/main/java/org/igt/utils/DataProviderUtils.java
+++ src/main/java/org/igt/utils/DataProviderUtils.java
@@ -19,7 +19,7 @@ import org.testng.annotations.DataProvider;
  */
 public class DataProviderUtils {
 	
-	protected static List<Map<String,String>> list = new ArrayList<>();   //Have to make it static otherwise it will not be called inside below method
+	protected static List<Map<String,String>> list = new ArrayList<>();   
 	
 	/**
 	 * This method will find out list of test cases<p>
@@ -28,13 +28,13 @@ public class DataProviderUtils {
 	 * @return
 	 */
 	@DataProvider(parallel=true)
-	public static Object[] getTestData(Method m) {   //This Method m is part of java reflect method which helps to identify which method is calling this method.
+	public static Object[] getTestData(Method m) {   
 		
-		String testcasename = m.getName();		//To get the name of the calling method
-		if(list.isEmpty()) {   //We make it conditional because it will be empty only first time after that it will not be called. Without this it will open this data sheet every time which will impact performance.
+		String testcasename = m.getName();		
+		if(list.isEmpty()) {   
 			list = ExcelUtils.getTestDetails(FrameworkConstants.getTestdatasheet());
 		}				
-		List<Map<String,String>> iterationlist = new ArrayList<>();	//This small list will be used to put values find out from excellist which are required for run i.e. only execute "yes" will be put in this list and then this list will be returned as part of this method.
+		List<Map<String,String>> iterationlist = new ArrayList<>();	
 		
 		for(int i=0;i<list.size();i++) {
 			if(list.get(i).get("testname").equalsIgnoreCase(testcasename) &&
@@ -42,7 +42,7 @@ public class DataProviderUtils {
 					iterationlist.add(list.get(i));
 				}								
 		}
-	//	list.removeAll(iterationlist); // We can keep this but directly removing data from list like this is not a good coding practice as whole index position will be changed. Anyway this line is also not required.
-		return iterationlist.toArray();		//As method is asking to return an array.
+	//	list.removeAll(iterationlist);  
+		return iterationlist.toArray();		
 	}
 }
