b4ce43d87b80a55220cc1534a820bfe222bacd2c
Mandeepsheoran <70449523+Mandeepsheoran@users.noreply.github.com>
2023-02-14 17:11:30 +0400

    Update PageModelAndPageFactoryTest.java

--- src/test/java/org/igt/seleniumframework/PageModelAndPageFactoryTest.java
+++ src/test/java/org/igt/seleniumframework/PageModelAndPageFactoryTest.java
@@ -11,8 +11,8 @@ import org.testng.annotations.Test;
 public class PageModelAndPageFactoryTest extends BaseTest {
 	WebDriver driver;
 	
-	@FrameworkAnnotations(author="Mandeep",category= {CategoryType.SMOKE},osinfo="Windows")   //We can pass author= {"Mandeep", "Anit") if you are using array for multiple value sin extentreport class
-	@Test(priority=1,enabled=true)   //@Test is annotation and parameter inside it are elements
+	@FrameworkAnnotations(author="Mandeep",category= {CategoryType.SMOKE},osinfo="Windows")  
+	@Test(priority=1,enabled=true)  
 	public void pageobjectmodeltest() {		
 		ExtentManager.getExtentTest()
 		         .assignAuthor("Mandeep").assignAuthor("Satish")
