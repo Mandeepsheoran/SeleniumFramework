af67785cca0d0cd23bf471fe1baf9c5514fb6fd6
Mandeepsheoran <70449523+Mandeepsheoran@users.noreply.github.com>
2023-02-14 16:52:27 +0400

    Update DriverManager.java

--- src/main/java/org/igt/drivers/DriverManager.java
+++ src/main/java/org/igt/drivers/DriverManager.java
@@ -16,18 +16,17 @@ public final class DriverManager {
 	private DriverManager() {		
 	}
 
-	private static ThreadLocal<WebDriver> dr = new ThreadLocal<>();  //We hv make this private as it is not getting used from outside
+	private static ThreadLocal<WebDriver> dr = new ThreadLocal<>(); 
 
 	public static WebDriver getDriver() {
 		return dr.get();
 	}	
-	//Change below method to deafult as only "Driver" class is using this which is only in this package.
-	static void setDriver(WebDriver driverref) { //This method is void because we are not getting anything
+	
+	static void setDriver(WebDriver driverref) { 
 		if(Objects.nonNull(driverref)) {
 			dr.set(driverref);
 		}
 	}
-	//Change below method to deafult as only "Driver" class is using this which is only in this package.
 	static void unload() {
 		dr.remove();
 	}
