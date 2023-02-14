b30a78ac27da45b4ba221351ce4492a270731058
Mandeepsheoran <70449523+Mandeepsheoran@users.noreply.github.com>
2023-02-14 17:00:51 +0400

    Update ConsoleLoggerImpl.java

--- src/main/java/org/igt/reports/ConsoleLoggerImpl.java
+++ src/main/java/org/igt/reports/ConsoleLoggerImpl.java
@@ -1,7 +1,5 @@
 package org.igt.reports;
 
-//This below code implementation is using Java 7 and we will not use it. Instaed of this, we will use "FarmeworkLogger" class
-//Class to prin logs on console, We will call this from BasePage class method liek clik method.
 public class ConsoleLoggerImpl implements Loggable {
 
 	@Override
